73192d7dd83fbe2988f8c64712e62a0b0584c244
EDF <velociraptor@ultrapulpo.com>
2025-04-06 19:27:48 -0700

    Add initial infra for polls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PollsListComponent } from './components/polls-list/polls-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PollsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
