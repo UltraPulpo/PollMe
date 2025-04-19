a6cb1691a604fd45760dcc6c4346d548255e82e4
EDF <velociraptor@ultrapulpo.com>
2025-04-19 09:45:29 -0700

    WIP - add crud operations

--- PollMeWeb/src/app/components/polls-list/polls-list.component.ts
+++ PollMeWeb/src/app/components/polls-list/polls-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Poll, PollService } from '../../services/poll.service';
+import { Poll } from '../../services/poll.service';
 
 @Component({
   standalone: true,
@@ -9,24 +9,6 @@ import { Poll, PollService } from '../../services/poll.service';
   templateUrl: './polls-list.component.html',
   styleUrl: './polls-list.component.scss'
 })
-export class PollsListComponent implements OnInit {
-  polls: Poll[] = [];
-
-  constructor(private pollService: PollService) { }
-
-  ngOnInit(): void {
-    this.loadPolls();
-  }
-
-  // Fetch all products from the API
-  loadPolls(): void {
-    this.pollService.getPolls().subscribe(
-      (data: Poll[]) => {
-        this.polls = data;
-      },
-      (error) => {
-        console.error('Error fetching polls', error);
-      }
-    );
-  }
+export class PollsListComponent {
+  @Input() polls: Poll[] = [];
 }
