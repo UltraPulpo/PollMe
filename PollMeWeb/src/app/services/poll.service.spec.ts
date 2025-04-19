435cfc33f3484df2204957e8dfa67fc58d569c2b
EDF <velociraptor@ultrapulpo.com>
2025-04-19 11:37:13 -0700

    Fix front-end tests and use chromium headless

--- PollMeWeb/src/app/services/poll.service.spec.ts
+++ PollMeWeb/src/app/services/poll.service.spec.ts
@@ -1,16 +1,36 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { PollService } from './poll.service';
 
 describe('PollService', () => {
   let service: PollService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PollService],
+    });
     service = TestBed.inject(PollService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify(); // Ensure no outstanding HTTP requests
+  });
+
+  it('should fetch polls', () => {
+    const mockPolls = [
+      { id: 1, name: 'Poll 1', description: 'Description 1' },
+      { id: 2, name: 'Poll 2', description: 'Description 2' },
+    ];
+
+    service.getPolls().subscribe((polls) => {
+      expect(polls).toEqual(mockPolls);
+    });
+
+    const req = httpMock.expectOne('https://localhost:7272/api/polls');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockPolls); // Provide mock response
   });
 });
